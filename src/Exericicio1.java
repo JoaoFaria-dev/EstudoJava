58102a78eac8af0b3f59d272f55434925f147dbf
jmaf4r1a-dev <joao.mfaria0211@gmail.com>
2025-08-22 21:07:05 -0300

    primeiro exercicio

--- src/Exericicio1.java
+++ src/Exericicio1.java
@@ -1,3 +1,17 @@
-public class Exericicio1 {
+import java.util.Scanner;
 
+public class Exericicio1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Digite o primeiro valor: ");
+        int valor1 = scan.nextInt();
+
+        System.out.println("Digite o segundo valor: ");
+        int valor2 = scan.nextInt();
+
+        int soma = valor1 + valor2;
+
+        System.out.println("O valor da soma desses valores é: " + soma);
+    }
 }
